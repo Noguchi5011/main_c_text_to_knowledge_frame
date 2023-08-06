966ef6ee7ea9ffd392ca41225896b7be77ecc201
Noguchi5011 <mi22a009@oecu.jp>
2023-08-06 16:24:10 +0900

    mistake_check.py add

---
+++ main_program/parse_mistake_check.py
@@ -0,0 +1,32 @@
+import json
+import os
+import dirtyjson
+
+file_path = "generate/correct_combined_results_4.json" 
+output_path = "generate/parsed_correct_output.json"
+
+with open(file_path, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# 2. `choices`の中の`arguments`を抽出し、Pythonの辞書に変換
+arguments = data[0]["choices"][0]["message"]["function_call"]["arguments"]
+parsed_arguments = dirtyjson.loads(arguments)
+
+formatted_data = {
+    "mistake_bool": parsed_arguments["mistake_bool"],
+    "explain_text": parsed_arguments["explain_text"],
+	"error_text": parsed_arguments["error_text"],
+    "correct_program": parsed_arguments["correct_program"].replace("\\\"", "\"").replace("\\\\", "\\")
+}
+
+# 4. JSONデータの後に続く間違ったコードを取得
+mistaken_code = data[1]
+
+# 5. 新しい形式のデータと間違ったコードを新しいJSONファイルに保存
+output_data = {
+    "mistake_info": formatted_data,
+    "mistaken_code": mistaken_code
+}
+
+with open(output_path, 'w', encoding='utf-8') as file:
+    json.dump(output_data, file, ensure_ascii=False, indent=4)
